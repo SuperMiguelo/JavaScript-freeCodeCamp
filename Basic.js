7c5be9ab8a2faf32a6f2e81492a62d411a811f9c
Michel <mi.leruyet@hotmail.fr>
2022-01-03 21:55:28 +0100

    im back

--- Basic.js
+++ Basic.js
@@ -1,3 +1,9 @@
 const myName = "Michel";
 const myStr = "My name is" + myName + "and I am well!";
 const myArray = ["dude", 3];
+
+function myFirstFunction (){
+    console.log("bruh");
+}
+
+myFirstFunction();
